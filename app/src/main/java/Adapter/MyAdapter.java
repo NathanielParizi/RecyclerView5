c3a8e2d664f2c31e14d4b95a9e222546686bc317
Nathaniel-Joel Parizi <forexmagnumopus@hushmail.com>
2017-10-25 12:11:48 -0400

    Initial commit

---
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -0,0 +1,71 @@
+package Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jtanuki.go.recyclerview5.R;
+
+import java.util.List;
+
+import Model.ListItem;
+
+/**
+ * Created by go on 10/25/2017.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context context;
+    private List<ListItem> listItems;
+
+    public MyAdapter(Context context, List listItems){
+
+        this.context = context;
+        this.listItems = listItems;
+
+    }
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+
+        ListItem item = listItems.get(position);
+
+    holder.name.setText(item.getName());
+        holder.description.setText(item.getDescription());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView name;
+        public TextView description;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/Model/ListItem.java
@@ -0,0 +1,37 @@
+package Model;
+
+/**
+ * Created by go on 10/25/2017.
+ */
+
+public class ListItem {
+
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public ListItem(String name, String description) {
+
+        this.name = name;
+        this.description = description;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
